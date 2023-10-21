98587c304d1fcb8afc4421cfc5c7621da25957fa
Gertjan van der Meulen <gertjan@hollands-groen.nl>
2023-10-21 22:38:15 +0200

    change some unittests

--- test/testactualparams.py
+++ test/testactualparams.py
@@ -1,6 +1,7 @@
 """ tests forward slashes are added """
 import unittest
-from resources import actualparams
+
+from resources.actualparams import create_actual_params
 
 
 class ActualParamsTest(unittest.TestCase):
@@ -10,13 +11,13 @@ class ActualParamsTest(unittest.TestCase):
         p1 = 'some   parameter'
         input_param = [p1]
         expect_param = '\\(\\"' + p1 + '\\"\\)'
-        self.assertEqual(actualparams.create_actual_params(input_param), expect_param)
+        self.assertEqual(create_actual_params(input_param), expect_param)
 
     def test_zero_param(self):
         """ Zero param test"""
         p1 = ''
         input_param = [p1]
-        self.assertEqual(actualparams.create_actual_params(input_param), '\\(\\"\\"\\)')
+        self.assertEqual(create_actual_params(input_param), '\\(\\"\\"\\)')
 
     def test_more_params(self):
         """ Multiple params test """
@@ -27,7 +28,7 @@ class ActualParamsTest(unittest.TestCase):
         input_param = [p1, p2, p3, p4]
         expect_param = '\\(\\"' + p1 + '\\",\\"' + str(p2) + '\\",\\"' + \
                        p3 + '\\",\\"' + p4 + '\\"\\)'
-        self.assertEqual(actualparams.create_actual_params(input_param), expect_param)
+        self.assertEqual(create_actual_params(input_param), expect_param)
 
 
 if __name__ == '__main__':
