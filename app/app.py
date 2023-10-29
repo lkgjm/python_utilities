2503a6006e64e4a02003e66ff15e964bc58a61bb
Gertjan van der Meulen <gertjan@hollands-groen.nl>
2023-10-29 22:00:58 +0100

    Add post method for forms

--- app/app.py
+++ app/app.py
@@ -1,23 +1,45 @@
 """ Store Data in redis"""
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+
 from redis import Redis
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '9f23e25f0b131d6df4004485c84d8db3936e0666bf4fdbde'
+
 bind_port = os.environ['BIND_PORT']
 REDIS_HOST = os.environ['REDIS_HOST']
 REDIS_PORT = os.environ['REDIS_PORT']
 
 redis = Redis(host=REDIS_HOST, port=REDIS_PORT)
+redis.incr('hits')
+total_hits = redis.get('hits').decode('utf-8')
+messages = [{'title': REDIS_HOST + ' on port ' + REDIS_PORT,
+             'content': total_hits}]
 
 
 @app.route('/', methods=["GET"])
-def home_page():
+def index():
     """ Get home page"""
-    redis.incr('hits')
-    total_hits = redis.get('hits').decode('utf-8')
-    return render_template('index.html', redis_db=REDIS_HOST, redis_count=total_hits)
+    return render_template('index.html', messages=messages)
+
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
 
 
 if __name__ == '__main__':
